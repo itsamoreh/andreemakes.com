2e62e95be7b2e84d5f3eb41dca76f08230270001
Amor Kumar <contact@amorkumar.com>
2023-08-01 20:02:52 -0400

    Customize fonts, logos

--- patterns/header.php
+++ patterns/header.php
@@ -7,12 +7,13 @@
  * Block Types: core/template-part/header
  */
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"32px","bottom":"32px","right":"24px","left":"24px"},"margin":{"top":"0px"}},"position":{"type":"sticky","top":"0px"}},"className":"site-header","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull site-header" style="margin-top:0px;padding-top:32px;padding-right:24px;padding-bottom:32px;padding-left:24px"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"nowrap","orientation":"horizontal"}} -->
-<div class="wp-block-group alignwide"><!-- wp:image {"id":49,"width":60,"sizeSlug":"full","linkDestination":"custom"} -->
-<figure class="wp-block-image size-full is-resized"><a href="/"><img src="<?php echo get_theme_file_uri( 'assets/images/logo.png' ); ?>" alt="" class="wp-image-49" width="60"/></a></figure>
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"32px","bottom":"32px","right":"24px","left":"24px"},"margin":{"top":"0px","bottom":"var:preset|spacing|xx-small"}},"position":{"type":"sticky","top":"0px"}},"backgroundColor":"white","textColor":"black","className":"site-header","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull site-header has-black-color has-white-background-color has-text-color has-background" style="margin-top:0px;margin-bottom:var(--wp--preset--spacing--xx-small);padding-top:32px;padding-right:24px;padding-bottom:32px;padding-left:24px"><!-- wp:group {"layout":{"type":"flex","justifyContent":"space-between","flexWrap":"nowrap","orientation":"horizontal"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":49,"width":214,"height":43,"sizeSlug":"full","linkDestination":"custom"} -->
+<figure class="wp-block-image size-full is-resized"><a href="/"><img src="<?php echo get_theme_file_uri( 'assets/images/logo.png' ); ?>" alt="" class="wp-image-49" width="214" height="43"/></a></figure>
 <!-- /wp:image -->
 
-<!-- wp:navigation {"ref":26} /--></div>
+<!-- wp:navigation {"ref":26,"layout":{"type":"flex","orientation":"horizontal","justifyContent":"left"}} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
